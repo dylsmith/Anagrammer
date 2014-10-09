a51f0d49191cda8bdc8f89b30069a613198c3a90
Dylan Smith <dilsmith@ucdavis.edu>
2014-10-09 15:40:27 -0700

    First working commit. Anagrammer runs, but is very slow. Many potential optimizations (ignoring word order and getting a better word set, for example). Fully commented.

--- Anagrammer.py
+++ Anagrammer.py
@@ -1,44 +1,38 @@
 from DictionaryToObject import ObjectToDictionary, WordToCharCount, test
 import copy
 
-def IsSubset(testWord, counts):
-    for char in testWord:
-        if char in counts:
-            counts[char] -= testWord[char]
-            if counts[char] == 0:
-                del counts[char]
-            elif counts[char] < 0:
+def IsSubset(testWord, counts):     # Given a test word and a set of counts, returns 0 if that word can't be made
+                                    # from those counts, or a new count set made by removing the necessary letters
+    for key in testWord:    # For each char in testWord
+        if key in counts:   # Check if that char is even in our count set - if not, return 0
+            counts[key] -= testWord[key]    # If it is, subtract the enough letters from counts to spell the word
+            if counts[key] == 0:    # If we have no more of that letter, delete it from our counts set
+                del counts[key]
+            elif counts[key] < 0:   # If we have too few of that letter, return 0; we can't spell this word
                 return 0
         else:
             return 0
-    return counts
+    return counts   # If we've successfully removed the necessary letters, return the count set of remaining letters
 
-def AnagrammerR(curString, counts):
-    origCounts = copy.deepcopy(counts)
-    if len(counts) == 0:
-        print curString
+def AnagrammerR(curString, counts): # Recursive call. curString increases with recursion depth,
+                                    # counts are the chars we still need to use
+    if len(counts) == 0:              # If we have no letters left to use
+        print 'Found: ',curString     # We have found an anagram
     else:
-        for word in dict:
-            counts = origCounts
-            viable = IsSubset(dict[word], counts)
-            if viable == 0:
+        for word in dict:       # For each potential word
+            viable = IsSubset(dict[word], copy.copy(counts))    # Check if it's a subset of the chars we have left.
+            if viable == 0:     # 0 means the word didn't fit
                 continue
-            else:
-                curString += word
-                AnagrammerR(curString, viable)
+            else:   # Otherwise, we have a new word for our sentence!
+                tempString = copy.copy(curString)   # Copy our curString
+                tempString += word + ' '            # And append the new word
+                AnagrammerR(tempString, copy.copy(viable))  # Recursively call Anagrammer, with this new
+                                                            # curString and set of remaining chard
 
+def Anagrammer(string): #I nitial front-facing call
+    AnagrammerR('', WordToCharCount(string))    # Call the recursive version, beginning with an empty string to add words to
 
-
-def Anagrammer(string):
-    AnagrammerR('', WordToCharCount(string))
-
-dict = ObjectToDictionary('Dictionary')
-
-#input = raw_input("Enter a sentence: ")
-input = "Hello"
-Anagrammer(input)
-
-#for word in dict:
-#    print word
-#    print dict[word]
+dict = ObjectToDictionary('Dictionary') # Load dictionary in from file
+input = raw_input("Enter a sentence: ") # Prompt user for a sentence
+Anagrammer(input)   # Call anagrammer
 
--- DictionaryToObject.py
+++ DictionaryToObject.py
@@ -1,36 +1,30 @@
 import cPickle as pickle
 
-def WordToCharCount(word):
-    counts = {}
-    word = word.lower()
-    for char in word:
-        if char.isalpha():
-            if char in counts:
-                counts[char] += 1
+def WordToCharCount(word):      # Maps from each char in the word to a count of its occurrences
+    counts = {}                 # Make the map
+    word = word.lower()         # Bring it to lowercase
+    for char in word:           # For each char
+        if char.isalpha():      # If it's  a letter,
+            if char in counts:  # And if it exists as a key in our map
+                counts[char] += 1   # Increment that char
             else:
-                counts[char] = 1
+                counts[char] = 1    # Else, create that key
     return counts
 
-def DictionaryToObject(fileName):
-    words = {}
-    with open(fileName, 'rt') as file:
-        for word in file:
-            words[word] = WordToCharCount(word)
+def DictionaryToObject(fileName):       # Reads from a plaintext dictionary file, writes it to a letter-counted binary one
+    words = {}                          # Map from word to its letter counts
+    with open(fileName, 'rt') as file:  # Open the plaintext
+        for word in file:               # For each word
+            word = word.rstrip()        # Remove whitespace/newlines
+            words[word] = WordToCharCount(word) # Generate the char counts
 
-    with open('Dictionary', 'wb') as out:
-        pickle.dump(words, out)
+    with open('Dictionary', 'wb') as out:   # Open the binary file
+        pickle.dump(words, out)             # Dump the map to it
 
-def ObjectToDictionary(fileName):
+def ObjectToDictionary(fileName):       # Loads a dictionary in from a file
     with open(fileName, 'rb') as file:
         return pickle.load(file)
 
-def test():
-    DictionaryToObject('wordsEn.txt')
-
-    dict = ObjectToDictionary('Dictionary')
-    for word in dict:
-        print word
-        print dict[word]
-
+#DictionaryToObject('wordsEn.txt') # Uncomment to write a new dictionary file
 
 
